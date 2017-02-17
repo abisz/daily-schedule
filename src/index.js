ebec35008030e90bfe35c8034ab2cf73855af8f0
Simon Reinsperger <reinsperger.smn@gmail.com>
2017-02-17 12:07:14 +0100

    feat: sort retrieved events, adds asnyc

--- src/index.js
+++ src/index.js
@@ -3,27 +3,37 @@
 const Calenduh = require('calenduh');
 const debug = require('debug')('daily-schedule');
 const moment = require('moment');
+const async = require('async');
 
-const log = console.log;
 const cal = new Calenduh(`${__dirname}/../client_secret.json`);
 
+const events = [];
+
 cal.calendarList().then((lists) => {
-  lists.forEach((list) => {
+  async.each(lists, (list, cb) => {
     cal.events(list.id, {
-      timeMin: moment().toISOString(),
-      timeMax: moment().add(1, 'day').toISOString(),
-    }).then((events) => {
+      timeMin: moment().startOf('day').toISOString(),
+      timeMax: moment().endOf('day').toISOString(),
+    }).then((eventsRetrieved) => {
       debug('Retrieved events');
-      events.forEach((e) => {
-        log(`${e.summary} (${moment(e.start.dateTime).format('HH:mm')} - ${moment(e.end.dateTime).format('HH:mm')})`);
-      });
+      events.push(...eventsRetrieved);
+      cb();
     }).catch((err) => {
       debug('Error in index.js cal.events');
       debug(err);
+      // can't pass error because async each would stop
+      cb();
+    });
+  }, () => {
+    debug('async.each callback');
+    const sorted = events.sort((a, b) => moment(a.start.dateTime) - moment(b.end.dateTime));
+    sorted.forEach((e) => {
+      process.stdout.write(
+        `${e.summary} (${moment(e.start.dateTime).format('HH:mm')}-${moment(e.end.dateTime).format('HH:mm')})\n`,
+      );
     });
   });
 }).catch((err) => {
   debug('Error while getting calendar lists');
   debug(err);
 });
-
